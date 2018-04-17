303d8dfec540bb226b746206408c4f91b72b2ccc
SerlockHolmes <1784955689@qq.com>
2018-04-17 22:19:44 +0800

    新增 注解 以及 包扫描功能

--- easy-core/src/main/java/com/yuyenews/core/load/LoadClass.java
+++ easy-core/src/main/java/com/yuyenews/core/load/LoadClass.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.yuyenews.core.annotation.Controller;
 import com.yuyenews.core.annotation.EasyBean;
 import com.yuyenews.core.util.ReadClass;
--- easy-core/src/main/java/com/yuyenews/core/util/FileUtil.java
+++ easy-core/src/main/java/com/yuyenews/core/util/FileUtil.java
@@ -1,13 +1,10 @@
 package com.yuyenews.core.util;
 
-import com.yuyenews.core.annotation.EasyBean;
-
 /**
  * 文件帮助
  * @author yuye
  *
  */
-@EasyBean(name="files")
 public class FileUtil {
 
 	/**
